65664eed251a21eae417c9b10046320ab0d728bd
Diana <dianaaranda14@gmail.com>
2023-05-18 10:29:28 -0700

    Counts and prints amount of tries

--- game.py
+++ game.py
@@ -10,11 +10,17 @@ def guessinggame():
     name = input("Whats your name?: ")
     randomnum = random.randint(1,100)
     guess = 0
+    tries = 0
     while randomnum != guess:
         guess = int(input("Guess a number between 1 and 100: "))
+        tries = tries + 1 
         if guess < randomnum:
             print("Guess too low")
         elif guess > randomnum:
             print("Guess too high")
+        else: 
+            print(f"Congrats, you got it in {tries} tries!")
+        
+        
     
 guessinggame()
